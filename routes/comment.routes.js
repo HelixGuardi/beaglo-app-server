258da8f455825560064082d820d69843b6ac0af5
Victor Hugo Guardiola <victorhugo.pessoal.guardiola@gmail.com>
2025-01-16 14:38:41 +0100

    comment routes DONE and WORKING

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -2,31 +2,37 @@ const express = require("express");
 const router = express.Router();
 
 const Comment = require("../models/Comment.model");
+const { verifyToken } = require("../middlewares/auth.middlewares");
 
-// POST /api/comments/:postId -> crea un comentario en un post especifico //! Pendiente de testear en postman
-router.post("/:postId", async (req, res, next) => {
+// POST /api/comments/posts/:postId -> crea un comentario en un post especifico
+router.post("/posts/:postId", verifyToken, async (req, res, next) => {
+
   try {
     await Comment.create({
       content: req.body.content,
-      user: req.body.user, //? Preguntar a Jorge
-      post: req.body.post, //? Preguntar a Jorge
+      user: req.payload._id,
+      post: req.params.postId
     });
     res.sendStatus(201);
+
   } catch (error) {
     next(error);
   }
 });
 
-// GET /api/comments/:postId -> obtiene comentarios de un post especifico //! Pendiente de testear en postman
-router.get("/:postId", async (req, res, next) => {
+// GET /api/comments/posts/:postId -> obtiene comentarios de un post especifico
+router.get("/posts/:postId", async (req, res, next) => {
   try {
-    //? Preguntar a Jorge
+
+    const response = await Comment.find({post: req.params.postId})
+    res.status(200).json(response)
+
   } catch (error) {
     next(error);
   }
 });
 
-// DELETE /api/comments/:commentId -> Elimina un comentario especifico //! Pendiente de testear en postman
+// DELETE /api/comments/:commentId -> Elimina un comentario especifico
 router.delete("/:commentId", async (req, res, next) => {
   try {
     await Comment.findByIdAndDelete(req.params.commentId);
